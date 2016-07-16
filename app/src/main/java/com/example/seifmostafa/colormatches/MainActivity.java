d7677c5a598dc6b14dc1dc5bb818bb9d1d2721e1
SeifMostafa <seifmostafa235@gmail.com>
2016-07-16 20:24:14 +0200

    2nd commit - twitter login done , nearly playground finished

--- app/src/main/java/com/example/seifmostafa/colormatches/MainActivity.java
+++ app/src/main/java/com/example/seifmostafa/colormatches/MainActivity.java
@@ -1,19 +1,67 @@
 package com.example.seifmostafa.colormatches;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Display;
+import android.widget.Toast;
+
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "hYH7oLmY0ddT41mBRmhhI5kjX";
+    private static final String TWITTER_SECRET = "QwJUPY03GaSzvjOQwieDGR75k8xGs8FAnLXaKGCxzaE9whdaXI";
+    private TwitterLoginButton loginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_main);
+        loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                // The TwitterSession is also available through:
+                // Twitter.getInstance().core.getSessionManager().getActiveSession()
+                TwitterSession session = result.data;
+                // TODO: Remove toast and use the TwitterSession's userID
+                // with your app's user model
+                String msg = "@" + session.getUserName() + " logged in! (#" + session.getUserId() + ")";
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+                startActivity(new Intent(MainActivity.this,PlaygroundActivity.class));
+            }
+            @Override
+            public void failure(TwitterException exception) {
+                Log.d("TwitterKit", "Login with Twitter failure", exception);
+            }
+        });
 
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Make sure that the loginButton hears the result from any
+        // Activity that it triggered.
+        loginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
     double getDimensions() {
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
@@ -27,5 +75,19 @@ public class MainActivity extends AppCompatActivity {
         double screenInches = Math.sqrt(x+y);
         Log.i("dimensions",String.valueOf(screenInches));
         return screenInches;
+    }
+    public static Boolean isTablet(Context context) {
+
+        if ((context.getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_LARGE) {
+
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
---
+++ app/src/main/java/com/example/seifmostafa/colormatches/PlaygroundActivity.java
@@ -0,0 +1,66 @@
+package com.example.seifmostafa.colormatches;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Random;
+import java.util.Stack;
+
+import static com.example.seifmostafa.colormatches.Utilities.*;
+
+public class PlaygroundActivity extends AppCompatActivity {
+
+    GridView gridView;
+    ImageButton imageButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playground);
+        gridView = (GridView)findViewById(R.id.gridview_playground);
+         BaseAdapter mListAdapter = new BaseAdapter() {
+             @Override
+            public int getCount() {
+                return 16;
+            }
+
+            @Override
+            public Object getItem(int i) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int i) {
+                return i+1;
+            }
+
+            @Override
+            public View getView(int i, View view, ViewGroup viewGroup) {
+                if (view == null) {
+                    view = getLayoutInflater().inflate(R.layout.layout_color, viewGroup, false);
+                    imageButton = (ImageButton) view.findViewById(R.id.imageButton_color);
+                    imageButton.setBackgroundColor(Utilities.getColor());
+                }
+                view.findViewById(R.id.imageButton_color).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(PlaygroundActivity.this,"Color Selected",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                return  view;
+            }
+        };
+        gridView.setAdapter(mListAdapter);
+    }
+
+}
+
+
